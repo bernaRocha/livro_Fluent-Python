55ffe472e8d0b19967a141ff88a8d39307e01a6a
Bernardo <mrochabernardo@gmail.com>
2022-08-31 17:47:54 -0300

    continuação do capítulo

--- capitulo_1-sequencia-de-cartas /main.py
+++ capitulo_1-sequencia-de-cartas /main.py
@@ -58,3 +58,18 @@ for card in reversed(deck):
 
 print(Card('Q', 'hearts') in deck) # True
 print(Card('7', 'beasts') in deck) # False
+
+# 
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+# Agora podemos listar as cartas em ordem crescente
+
+for card in sorted(deck, key=spades_high):
+    print(card)  
+
+print(' '* 40)
